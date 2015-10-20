b6ae63495afcac13de6e2850e60d496a0c91bf77
Michael Van Veen <michael@mvanveen.net>
2015-10-20 12:25:20 -0700

    Add osx platform detection

--- platform.sh
+++ platform.sh
@@ -1,11 +1,13 @@
 platform='unknown';
 unamestr=`uname`;
 
-#TODO(mvv): check uname for OS X
 if [[ "$unamestr" == 'Linux' ]]; then
    platform='linux'
    #TODO(mvv): install things other than ubuntu
    source ubuntu.sh
+elif [[ "$unamestr" == 'Darwin' ]]; then
+   platform='osx'
+   source osx.sh
 fi
 
 success "detected & configured platform: $platform"
